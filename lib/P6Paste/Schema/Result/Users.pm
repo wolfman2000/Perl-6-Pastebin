3e024aa5227361deea075781606761fc48c8dcd6
Jason Felds <jafelds@gmail.com>
2009-11-27 02:12:39 +0100

    Add pod. Didn't see much coverage difference though.

--- lib/P6Paste/Schema/Result/Users.pm
+++ lib/P6Paste/Schema/Result/Users.pm
@@ -51,3 +51,54 @@ __PACKAGE__->has_many('pastes' => 'P6Paste::Schema::Result::Pastes',
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
+
+__END__
+
+=head1 DESCRIPTION
+
+The users table contains the list of all of the users that have
+been recognized within the system. Users can either be
+registered or not.
+
+=head1 COLUMNS
+
+=head2 id
+
+This is the traditional primary id column.
+
+=head2 uname
+
+This is the username of the person. 32 IRC compatible characters
+are allowed. This field is required.
+
+=head2 email
+
+This field is required if the user wishes to register. The
+email address (up to 320 charaters) must be valid.
+
+=head2 pword
+
+This field is required if the user wishes to register. This
+is a hashed/salted field.
+
+=head2 created
+
+This field shows when the nickname was first created. This
+could be useful for record keeping.
+
+=head1 SEE ALSO
+
+=head2 L<Catalyst> - The Catalyst framework
+
+=head2 L<DBIx::Class> - The DBIC framework
+
+=head1 AUTHOR
+
+Jason Felds
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+    
+=cut
