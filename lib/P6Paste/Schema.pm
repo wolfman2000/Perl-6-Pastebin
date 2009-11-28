a1069d673df950f5cc73a83f1d6ec5f51a12f2d7
Jason Felds <jafelds@gmail.com>
2009-11-28 16:45:21 +0100

    Add POD.

--- lib/P6Paste/Schema.pm
+++ lib/P6Paste/Schema.pm
@@ -14,3 +14,54 @@ __PACKAGE__->load_namespaces;
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
+
+=head1 DESCRIPTION
+
+This file loads all of the tables from the database.
+
+=head1 TABLES
+
+=over
+
+=item L<P6Paste::Schema::Result::Users>
+
+This is the table of users.
+
+=item L<P6Paste::Schema::Result::Pastes>
+
+This is the table of pastes submitted.
+
+=item L<P6Paste::Schema::Result::Tags>
+
+This is the table of searchable tags (to be fully implemented later).
+
+=item L<P6Paste::Schema::Result::PasteTags>
+
+This table links Pastes and Tags.
+
+=item L<P6Paste::Schema::Result::MessCategories>
+
+This table contains message categories for displaying messages.
+
+=item L<P6Paste::Schema::Result::Messages>
+
+This table contains the messages to be displayed at certain times.
+
+=back
+
+=head1 SEE ALSO
+
+=head2 L<Catalyst> - The Catalyst framework
+
+=head2 L<DBIx::Class> - The DBIC framework
+
+=head1 AUTHOR
+
+Jason Felds
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+    
+=cut
