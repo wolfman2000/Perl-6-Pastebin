cbbb437baa1576acea02fd7886460b58dabbcbfa
Jason Felds <jafelds@gmail.com>
2009-11-28 05:49:37 +0100

    Add some of the precompiled data.

--- dbreload.bash
+++
@@ -1,1 +0,0 @@
-perl script/p6paste_create.pl model DBIC DBIC::Schema P6Paste::Schema create=static dbi:SQLite:p6paste.db
--- dbreload.pl
+++ dbreload.pl
@@ -4,7 +4,50 @@ use FindBin;
 use lib "$FindBin::Bin/lib";
 use P6Paste::Schema;
 
-my $schema = P6Paste::Schema->connect('dbi:SQLite:dbname=foo.db');
+my $schema = P6Paste::Schema->connect('dbi:SQLite:dbname=p6paste.db');
 
 $schema->deploy();
+
+$schema->resultset("MessCategories")->populate([
+	{
+		id => 1, name => 'regerror', messages => [
+			{message => 'One day, we will have our group of infinite monkeys make a better version of this code.'},
+			{message => 'I apologize. Ada unleashed her Python (or was it ASP?) which Bashed into the Perl code with his Ruby teeth. See? Sharp fangs.'},
+			{message => 'We are sorry. Your registration could not be completed as dialed. Please check the URL and try again.'},
+		],
+	},
+	{
+		id => 2, name => 'regsuccess', messages => [
+			{message => 'Howdy friend! How would you like to be neighbors? Come jo--wait, you just did already! Nevermind. :)'},
+			{message => 'Welcome to the neighborhood.'},
+			{message => 'Ah, a new face. Welcome to the pastebin.'},
+		],
+	},
+	{
+		id => 3, name => 'pasteerror', messages => [
+			{message => 'Crap...must have left the glue somewhere else. Hang on.'},
+			{message => 'Please do not make me have to use duct tape for a pastebin.'},
+			{message => 'We need an infinite amount of butterflies at this rate...'},
+		],
+	},
+	{
+		id => 4, name => 'pasteanon', messages => [
+			{message => 'Ah, a new kid. Do not be afraid: we do not bite. Much. :)'},
+			{message => 'Thanks for your contribution...who are you again? You must be new.'},
+		],
+	},
+	{
+		id => 5, name => 'pasteuser', messages => [
+			{message => 'Just a little more...got it! Your paste is saved.'},
+			{message => 'The butterflies in the back room thank you for contributing.'},
+		],
+	},
+	{
+		id => 6, name => 'pviewbadid', messages => [
+			{message => 'I think we were looking into our telescope backwards or something...'},
+			{message => 'Dag blasted pirates! They gave us a fake map! We cannot find the booty!'},
+			{message => 'This is not good...the GPS is out of power!'},
+		],
+	},
+]);
 
