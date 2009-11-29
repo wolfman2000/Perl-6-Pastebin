d5438bf04390692091b83700633650ccaeea992e
Jason Felds <jafelds@gmail.com>
2009-11-29 17:40:14 +0100

    Fix MYAPP_DSN assignment.

--- coverage.bash
+++ coverage.bash
@@ -1,1 +1,1 @@
-CATALYST_DEBUG=0 DBIC_TRACE=0 TEST_POD=1 MYAPP_DSN="dbi:SQLite:myappTEST.db" cover -test -outputdir "./root/static/cover_db"
+CATALYST_DEBUG=0 DBIC_TRACE=0 TEST_POD=1 MYAPP_DSN="myappTEST.db" cover -test -outputdir "./root/static/cover_db"
