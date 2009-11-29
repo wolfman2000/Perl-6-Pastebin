00ea132d69cc698f8b1a8ab6be9232909828f1ec
Jason Felds <jafelds@gmail.com>
2009-11-29 03:15:44 +0100

    Fix ResultSet. Update coverage.

--- lib/P6Paste/Schema/ResultSet/Users.pm
+++ lib/P6Paste/Schema/ResultSet/Users.pm
@@ -7,7 +7,8 @@ use parent 'DBIx::Class::ResultSet';
 sub get_name
 {
     my ($self, $uid) = @_;
-    return $self->find($uid, {select => ['uname']})->uname;
+    my $row = $self->find($uid, {select => ['uname']});
+    return defined $row ? $row->uname : undef;
 }
 
 1;
